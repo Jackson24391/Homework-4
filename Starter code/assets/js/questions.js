441dd2f78e4389e3c1bc5f975226493d8c85839d
Jackson24391 <131276854+Jackson24391@users.noreply.github.com>
2023-05-27 12:13:44 -0400

    Initial Commit

---
+++ Starter code/assets/js/questions.js
@@ -0,0 +1,28 @@
+
+var questions = [
+  {
+    title: 'What word is spelled incorrectly in every single dictionary?',
+    choices: ['Incorrectly', 'Dictionary', 'Supercalifragilisticexpialidocious', 'Lateish'],
+    answer: 'Incorrectly',
+  },
+  {
+    title: 'What is it that lives if it is fed, and dies if you give it a drink?',
+    choices: ['A Bank', 'A Fire', 'A Fish', 'Sourdough Starter'],
+    answer: 'A Fire',
+  },
+  {
+    title: 'If a plane crashes on the border between the United States and Canada, where do they bury the survivors?',
+    choices: ['United States', 'Canada', 'Survivors are not buried', 'Right on the border'],
+    answer: 'Survivors are not buried',
+  },
+  {
+    title: 'You spot a boat full of people but there is not a single person on board. How is that possible?',
+    choices: ['They are made out of cardboard', 'Its a hologram', 'Hallucination', 'Everyone on board is married'],
+    answer: 'Everyone on board is married',
+  },
+  {
+    title: 'What has a face and two hands, but no arms or legs?',
+    choices: ['A clock', 'A Snake', 'A Worm', 'A Walrus'],
+    answer: 'A clock',
+  },
+];
